b0e9719e99c6248a805108df12d111a384fff0d1
Eudora <thebabyalien540@gmail.com>
2026-04-27 16:39:07 +0700

    Update touch module loader script for new modules

--- recovery/root/system/bin/load-touch-module.sh
+++ recovery/root/system/bin/load-touch-module.sh
@@ -1,15 +1,26 @@
+#!/sbin/sh
+# Touch module loader for tanzanite
+# Written by @linastorvaldz on GitHub
+# Updated by @ramabondanp on GitHub
+# Last Update: 2025/12/05
+
+# disable exit-on-error
 set +e
-mod_dir="/vendor_dlkm/lib/modules"
-modules=(
-  focaltech_touch_rodin.ko
-  xiaomi_touch_rodin.ko
-  goodix_core_rodin.ko
-  xiaomi_spi_tee.ko
+
+mod_dir="/lib/modules"
+mods=(
+  switch_class.ko # has no depends
+  lct_tp.ko # has no depends
+  mtk-afe-external.ko # has no depends
+  xiaomi.ko # has no depends
+  stub_scp.ko # stub module to export scp_ipidev symbol
+  ft3519t.ko # finally, load the touch driver after loading its dependencies
+  focaltech_touch_rodin.ko # add for rodin
+  xiaomi_touch_rodin.ko # add for rodin
+  goodix_core_rodin # add for rodin
 )
 
-mount /vendor_dlkm
-# load modules
-for module in "${modules[@]}"; do
-  insmod $mod_dir/$module
+# load the modules
+for m in ${mods[@]}; do
+  insmod $mod_dir/$m
 done
-umount /vendor_dlkm
